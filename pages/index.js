6de44829e4582df08cea7d52a247fb917a776418
Goiar-uxui <email-de-la-cuenta-goiar-uxui@gmail.com>
2026-07-02 15:31:10 -0300

    fix: static HTML sin Next.js

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,1 @@
-export async function getServerSideProps({ res }) {
-  const fs = require('fs');
-  const path = require('path');
-  const html = fs.readFileSync(path.join(process.cwd(), 'public', 'portal.html'), 'utf8');
-  res.setHeader('Content-Type', 'text/html');
-  res.write(html);
-  res.end();
-  return { props: {} };
-}
-
 export default function Page() { return null; }
